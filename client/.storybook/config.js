102f9191a9ae717d1a125ef65f9e26bf5585948f
followthewhiterabbit <3tonishere@gmail.com>
2020-02-12 19:29:02 +0300

    Introduce storybook-addon-jsx plugin for storybook

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,5 +1,13 @@
-import { addDecorator } from '@storybook/react'
+import { addDecorator, addParameters } from '@storybook/react'
+import { jsxDecorator } from 'storybook-addon-jsx'
 
 import StylesDecorator from './styles-decorator'
 
 addDecorator(StylesDecorator)
+addDecorator(jsxDecorator)
+
+addParameters({
+  jsx: {
+    skip: 2,
+  },
+})
